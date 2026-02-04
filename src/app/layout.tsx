526b016cce619b34bd07facd6da14a24302aae71
ucup sarucup <ucupsarucup669@gmail.com>
2026-02-04 07:12:48 +0000

    theres an error invalid DOM property 'crossorigin'. DId you mean 'crossO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from 'next';
 import './globals.css';
 import {SidebarProvider, SidebarInset} from '@/components/ui/sidebar';
@@ -19,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background">
