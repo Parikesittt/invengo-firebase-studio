9eed6f555a4d362b9242a245a6820989a15f0d58
ucup sarucup <ucupsarucup669@gmail.com>
2026-02-05 06:29:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4870784624-1b500",
+  "appId": "1:761557737993:web:e7830bdd550b390d8b0ed8",
+  "apiKey": "AIzaSyD7UAw6y5Aw-74g3-gaRqMXo0wQBOqkrvU",
+  "authDomain": "studio-4870784624-1b500.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "761557737993"
+};
